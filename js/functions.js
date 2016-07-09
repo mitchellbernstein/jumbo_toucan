00d9290fec0fd6eed799e4a5bbcc3f34787af562
Mitchell Bernstein <mitchellbernstein@mac.com>
2016-07-09 17:37:14 -0400

    BOOYA BITCH- Redesigne hero-parallax to be cool and hip
    Took a while, but the hero image is getting closer. Need to add the
    email form, title, and a little blurb. But the hero parallax for
    desktop is near bootyful.

--- js/functions.js
+++ js/functions.js
@@ -9,15 +9,27 @@ $(window).scroll(function(){
 	});
 	// Controls Watch
 	$(".watch-1").css({
-		"transform" : "translate(0px, -"+ wScroll /6 +"%)"
+		"transform" : "translate(0px, -"+ wScroll /20 +"%)"
 	});
 	// Controls Square shape
 	$(".shape-1").css({
-		"transform" : "translate(0px, -"+ wScroll /40 +"%)"
+		"transform" : "translate(0px, -"+ wScroll /50 +"%)"
 	});
 	// Controls cicle shape
 	$(".shape-2").css({
-		"transform" : "translate(0px, -"+ wScroll /160 +"%)"
+		"transform" : "translate(0px, -"+ wScroll /40 +"%)"
+	});
+	// Controls Square shape
+	$(".shape-3").css({
+		"transform" : "translate(0px, -"+ wScroll /30 +"%)"
+	});
+	// Controls cicle shape
+	$(".shape-4").css({
+		"transform" : "translate(0px, -"+ wScroll /50 +"%)"
+	});
+	// Controls cicle shape
+	$(".shape-5").css({
+		"transform" : "translate(0px, -"+ wScroll /80 +"%)"
 	});
 
 });
