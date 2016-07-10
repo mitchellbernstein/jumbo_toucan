d1eea56b5193ad5ffc13a6cb0752ffc0933fc471
Mitchell Bernstein <mitchellbernstein@mac.com>
2016-07-09 21:00:06 -0400

    Smaller fixes
    Fixed website nav, some parallax spacing, added 4 points, created two
    form styles, need to fix profile section—big photo, and contact
    section. then responsiveness. Responsive design already half done too.

--- js/functions.js
+++ js/functions.js
@@ -9,28 +9,38 @@ $(window).scroll(function(){
 	});
 	// Controls Watch
 	$(".watch-1").css({
+		"transform" : "translate(0px, -"+ wScroll /28 +"%)"
+	});
+	
+	$(".shape-1").css({
+		"transform" : "translate(0px, -"+ wScroll /160 +"%)"
+	});
+
+	$(".shape-2").css({
 		"transform" : "translate(0px, -"+ wScroll /20 +"%)"
 	});
-	// Controls Square shape
-	$(".shape-1").css({
-		"transform" : "translate(0px, -"+ wScroll /50 +"%)"
-	});
-	// Controls cicle shape
-	$(".shape-2").css({
-		"transform" : "translate(0px, -"+ wScroll /40 +"%)"
-	});
-	// Controls Square shape
+	
 	$(".shape-3").css({
-		"transform" : "translate(0px, -"+ wScroll /30 +"%)"
+		"transform" : "translate(0px, -"+ wScroll /16 +"%)"
 	});
-	// Controls cicle shape
+
 	$(".shape-4").css({
-		"transform" : "translate(0px, -"+ wScroll /50 +"%)"
-	});
-	// Controls cicle shape
-	$(".shape-5").css({
 		"transform" : "translate(0px, -"+ wScroll /80 +"%)"
 	});
+
+	$(".shape-5").css({
+		"transform" : "translate(0px, -"+ wScroll /320 +"%)"
+	});
+
+	//Look at how far .cloths-pics is from the top of the DOM
+	if(wScroll > $(".clothes-pics").offset().top - ($(window).height() / 1.2)) {
+		// Every element inside .cloths-pics will run this funciton
+		$(".clothes-pics figure").each(function(i) {
+			setTimeout(function(){
+				$(".clothes-pics figure").eq(i).addClass("is-showing");
+			}, 150 * (i+1));
+		});
+	}
 
 });
 
